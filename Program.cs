9e26c4f102df45a9822200cf1114f746b14406b3
dmannix <dmannix1103@gmail.com>
2021-07-27 18:51:39 +1000

    fixed some variable names

--- Program.cs
+++ Program.cs
@@ -8,15 +8,17 @@ namespace shamir
         static void Main(string[] args)
         {
 		BigInteger secret = new BigInteger(666);
-		int degree = 4;
-		BigInteger prime = new BigInteger(15887);
-		ShamirSecretSharing sss = new ShamirSecretSharing(degree,prime,secret);	
+		Console.WriteLine("secret is {0}",secret);
 		
-		int threshold = 5;
-		Share[] shares = sss.GenerateShares(threshold);
+		int threshold = 4;
+		BigInteger prime = BigInteger.Parse("130922996888451848749185915881459813412881");
+		ShamirSecretSharing sss = new ShamirSecretSharing(threshold,prime,secret);	
+		
+		int n_shares = 5;
+		Share[] shares = sss.GenerateShares(n_shares);
 		
 		BigInteger recovered = sss.Interpolate(shares);
-		Console.WriteLine("secret is {0}",recovered);
+		Console.WriteLine("recovered secret is {0}",recovered);
 	}
     }
 }
